c10a5d22862c0aa0969f05c6a4980c8006c8782a
andbuzzo <andrelbuzzo@gmail.com>
2022-05-01 12:56:14 -0300

    Instância de forma automatica

--- src/main/java/com/andre/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/andre/bookstore/repositories/LivroRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Livro extends JpaRepository<com.andre.bookstore.domain.Livro, Integer> {
+public interface LivroRepository extends JpaRepository<com.andre.bookstore.domain.Livro, Integer> {
 
 }
