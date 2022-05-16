8e85cd5d0299c99007c0da0bfae91b1d74076ed0
andbuzzo <andrelbuzzo@gmail.com>
2022-05-16 15:12:11 -0300

    findAll para Categoria utilizando padrao DTO

--- src/main/java/com/andre/bookstore/service/CategoriaService.java
+++ src/main/java/com/andre/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.andre.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,9 @@ public class CategoriaService {
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
+	}
+	
 }
