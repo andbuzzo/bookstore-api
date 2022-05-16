01fb1a1fd42e3475e59bcc84a525a01eedbe56f9
andbuzzo <andrelbuzzo@gmail.com>
2022-05-16 01:19:51 -0300

    Perfil de dev criado

--- src/main/java/com/andre/bookstore/service/DBservice.java
+++ src/main/java/com/andre/bookstore/service/DBservice.java
@@ -27,14 +27,15 @@ public class DBservice {
 		Categoria cat3 = new Categoria(null, "biografia", "Livros de biografia");
 		
 		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
-		Livro l2 = new Livro(null, "Quimica", "Escola de Quimica", "Quimica", cat2);
-		Livro l3 = new Livro(null, "Minha Biografia", "André", "André", cat3);
+		Livro l2 = new Livro(null, "Quimica", "Escola de Quimica", "Quimica", cat1);
+		Livro l3 = new Livro(null, "Minha Biografia", "André", "André", cat2);
 		Livro l4 = new Livro(null, "eletrica já", "escola de eletrica", "eletrica", cat2);
 		
-		cat1.getLivros().addAll(Arrays.asList(l1,l2,l3,l4));
+		cat1.getLivros().addAll(Arrays.asList(l1,l2));
+		cat2.getLivros().addAll(Arrays.asList(l3,l4));
 		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1,cat2, cat3));
-		this.livroRespository.saveAll(Arrays.asList(l1, l2, l3, l4));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2, cat3));
+		livroRespository.saveAll(Arrays.asList(l1, l2, l3, l4));
 	}
 
 }
