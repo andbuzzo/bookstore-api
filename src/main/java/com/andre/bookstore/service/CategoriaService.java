c041d4c5e85123500d83da6676ec255930513f4e
andbuzzo <andrelbuzzo@gmail.com>
2022-05-16 15:36:51 -0300

    update para classe categoria

--- src/main/java/com/andre/bookstore/service/CategoriaService.java
+++ src/main/java/com/andre/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andre.bookstore.domain.Categoria;
+import com.andre.bookstore.dtos.CategoriaDTO;
 import com.andre.bookstore.repositories.CategoriaRepository;
 import com.andre.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -29,6 +30,14 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
 		return repository.save(obj);
 	}
 	
